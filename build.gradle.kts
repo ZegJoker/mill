f03e5c760045920a75a95205343de85706ee2181
Stanley <stanley.coder@gmail.com>
2023-10-14 00:03:42 +0800

    ci: add github workflows

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,21 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform).apply(false)
     alias(libs.plugins.composeMultiplatform).apply(false)
 }
+
+subprojects {
+    group = "coder.stanley.mill"
+    plugins.withId("maven-publish") {
+        configure<PublishingExtension> {
+            repositories {
+                maven {
+                    name = "MillRepo"
+                    setUrl("https://maven.pkg.github.com/ZegJoker/mill")
+                    credentials {
+                        username = System.getenv("MILL_REPO_USERNAME")
+                        password = System.getenv("MILL_REPO_PUBLISH_KEY")
+                    }
+                }
+            }
+        }
+    }
+}
